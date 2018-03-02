6667733a84fa965bd835fb5158864825dc32de66
sebsel <sebsel@seblog.nl>
2018-03-02 18:22:27 +0100

    add zsh

---
+++ zsh/aliases.zsh
@@ -0,0 +1,14 @@
+# === Git ===
+
+alias gs="git status"
+alias gl="git log"
+alias gcom="git checkout master"
+alias gaa="git add ."
+alias gc="git commit -m "
+alias gp="git push"
+alias nah="git reset --hard && git clean -df"
+
+# === Laravel ===
+
+alias art="php artisan"
+
