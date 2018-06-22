cf42400d2774f28d28fdebded16c64edf05ffe07
Sebastiaan Andeweg <sebastiaan@e-cube.nl>
2018-06-22 20:01:46 +0200

    add git pull command

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,11 +1,15 @@
 # === Git ===
 
+# note to self: there are also aliases from the oh-my-zsh plugin
+# github.com/robbyrussell/oh-my-zsh/blob/master/plugins/git/git.plugin.zsh
+
 alias gs="git status -bs"
 alias gl="git log"
 alias gcom="git checkout master"
 alias gaa="git add ."
 alias gc="EDITOR=vim git commit -v"
 alias gp="git push"
+alias gpl="git pull"
 alias nah="git reset --hard && git clean -df"
 alias git-hash="git rev-parse --short HEAD"
 alias git-init="git init && gaa && git commit -q"
