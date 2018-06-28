dd66b4a0e388d6821c2f11f831e2c42eab74d93d
sebsel <sebsel@seblog.nl>
2018-06-28 18:41:48 +0200

    add bash command to switch php versions

--- php/aliases.zsh
+++ php/aliases.zsh
@@ -8,3 +8,20 @@ phan() {
     return $?
 }
     
+# easily switch php-versions with brew
+swphp() {
+    current_php=$(ls -l `which php` | grep -o 'php@\d\.\d' || echo 'php')
+    desired_php="php@$1"
+
+    if [ "$current_php" = "$desired_php" ]; then
+        echo "already using $desired_php"
+        return
+    fi
+
+    echo "switching from $current_php to $desired_php"
+
+    # my homebrew runs under one user, but I have multiple
+    sudo -u seb brew unlink $current_php
+    sudo -u seb brew link $desired_php --force
+}
+
