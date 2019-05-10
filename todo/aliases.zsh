43b08424d48d1557d73937063fb3ce932e97cb0f
Sebastiaan Andeweg <seb@seblog.nl>
2019-05-10 11:46:27 +0200

    add shortcut to todo-list
    This also fixes a bug where `todo` without arguments would add and empty
    task.

--- todo/aliases.zsh
+++ todo/aliases.zsh
@@ -1,6 +1,6 @@
 alias today=$DOTFILES/todo/today.zsh
 alias now="today `date +%H:%M`:"
-alias todo='today [ ] '
 alias todo-list="ag --nobreak --nocolor --nonumbers '^\s*\- \[ \]' ~/notes/*"
 alias todo-vlist="vim \`todo-list -l\`"
+function todo { if [ -n "$*" ]; then; today "[ ] $*"; else; todo-list; fi }
 
