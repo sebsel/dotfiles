71ebb940e34324a413bf95348949f7860a061d2b
Sebastiaan Andeweg <seb@seblog.nl>
2019-06-21 19:41:22 +0200

    fix today alias

--- todo/aliases.zsh
+++ todo/aliases.zsh
@@ -1,5 +1,5 @@
 alias today=$DOTFILES/todo/today.zsh
-alias now="today `date +%H:%M`:"
+alias now="today \`date +%H:%M\`:"
 alias todo-list="ag --nobreak --nocolor --nonumbers '^\s*\- \[ \]' ~/notes/*"
 alias todo-vlist="vim \`todo-list -l\`"
 function todo { if [ -n "$*" ]; then; today "[ ] $*"; else; todo-list; fi }
