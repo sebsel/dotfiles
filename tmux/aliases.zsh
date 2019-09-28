0618804e71aad413502a5c92d42525d5820a442a
Sebastiaan Andeweg <seb@seblog.nl>
2019-09-28 08:25:00 +0200

    tweak tmux session switcher

--- tmux/aliases.zsh
+++ tmux/aliases.zsh
@@ -7,17 +7,16 @@ alias lss="tmux ls"
 # https://github.com/szymonkaliski/dotfiles/blob/ad449834cedf28d026bbab0c57413d049c7dc45d/Dotfiles/zshrc#L605-L621
 # but changed to work with windows instead of panes
 function ftwindow() {
-  local windows target
+  local sessions target
 
-  windows=$(tmux list-windows -a -F '#S;###I;#{window_name};#{pane_current_path}')
-  target=$(echo "$windows" | column -t -s ";" | fzf --select-1 --exit-0 --query="$1" --prompt="tmux > ") || return
+  sessions=$(tmux list-sessions -F '#S;#W;#{window_panes};#{pane_current_path}')
+  target=$(echo "$sessions" | column -t -s ";" | fzf --select-1 --exit-0 --query="$1" --prompt="tmux > ") || return
 
   local target_session=$(echo $target | cut -d " " -f1)
-  local target_window=$(echo $target | cut -d "#" -f2 | cut -d " " -f1)
 
   if [ -z "$TMUX" ]; then
-    tmux attach-session -t "$target_session" \; select-pane -t "$target_window"
+    tmux attach-session -t "$target_session"
   else
-    tmux switch-client -t "$target_session:$target_window"
+    tmux switch-client -t "$target_session"
   fi
 }
