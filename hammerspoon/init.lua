4e7c9b952157f8b9ea4d29f126fe9b79db71a63a
Sebastiaan Andeweg <seb@seblog.nl>
2021-04-04 19:14:28 +0200

    playing with hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,33 @@
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.alert.show("Hello World!")
+-- apps
+terminal = 'Alacritty'
+
+-- Keys
+hyper = {"ctrl", "alt", "cmd"}
+
+-- Window Halfs and Thirds
+hs.loadSpoon("WindowHalfsAndThirds")
+spoon.WindowHalfsAndThirds:bindHotkeys({
+  left_half   = { hyper, "H" },
+  right_half  = { hyper, "L" },
+  third_left  = { hyper, "J" },
+  third_right = { hyper, "K" },
+  max_toggle  = { hyper, ";" },
+  undo        = { hyper, "U" },
+  larger      = { hyper, "N" },
+  smaller     = { hyper, "M" },
+})
+
+-- Terminal
+hs.application.enableSpotlightForNameSearches(true)
+terminalOpen = true
+hs.hotkey.bind({"cmd"}, "Escape", function()
+  hs.application.launchOrFocus(terminal)
+  app = hs.application.find(terminal)
+  if terminalOpen then
+    terminalOpen = false
+    app:unhide()
+  else
+    terminalOpen = true
+    app:hide()
+  end
 end)
