22ad55effbbd964b7206039e626b1025a093dd4a
Sebastiaan Andeweg <seb@seblog.nl>
2021-04-04 17:51:58 +0200

    update Elixir language server
    I probably should not commit this here.

--- bin/elixir-ls/language_server.sh
+++ bin/elixir-ls/language_server.sh
@@ -1,18 +1,9 @@
-#!/usr/bin/env sh
+#!/bin/sh
 # Launches the language server. This script must be in the same directory as the compiled .ez archives.
 
-readlink_f () {
-  cd "$(dirname "$1")" > /dev/null
-  filename="$(basename "$1")"
-  if [ -h "$filename" ]; then
-    readlink_f "$(readlink "$filename")"
-  else
-    echo "`pwd -P`/$filename"
-  fi
-}
+dir=$(dirname "$0")
 
-SCRIPT=$(readlink_f $0)
-SCRIPTPATH=`dirname $SCRIPT`
-export ERL_LIBS="$SCRIPTPATH:$ERL_LIBS"
+export ELS_MODE=language_server
+export ELS_SCRIPT="ElixirLS.LanguageServer.CLI.main()"
 
-elixir -e "ElixirLS.LanguageServer.CLI.main()"
+exec "${dir}/launch.sh"
