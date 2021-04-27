c01301ce6e66d55370ca31494a78b2513ff2dab4
Sebastiaan Andeweg <seb@seblog.nl>
2021-04-27 14:37:52 +0200

    add tool to grab words for Monkeytype.com
    Just run it like: `woorden ijk | pbcopy` and let Monkeytype do the randomization.

--- tools/aliases.zsh
+++ tools/aliases.zsh
@@ -2,3 +2,10 @@ alias dotenv="env \$(cat .env | xargs) "
 alias miex="iex -S mix"
 alias b64="cat | base64 -D"
 alias toolbox="node ~/code/compose/toolbox/toolbox.js"
+
+function woorden {
+  # Download from https://www.opentaal.org/bestanden/file/2-woordenlijst-v-2-10g-bronbestanden
+  cat ~/Documents/OpenTaal-210G-woordenlijsten/OpenTaal-210G-basis-gekeurd.txt |
+    grep $1 |
+    awk '{printf $0 " "}'
+}
