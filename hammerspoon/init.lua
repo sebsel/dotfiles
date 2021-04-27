802ad89114ecdb4bc5b7c752e860e5a5bdb6c70f
Sebastiaan Andeweg <seb@seblog.nl>
2021-04-27 14:32:21 +0200

    toggle all the things

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -35,32 +35,35 @@ hs.hotkey.bind(hyper, "H", function() hs.window.filter.focusWest() end)
 hs.hotkey.bind(hyper, "J", function() hs.window.filter.focusSouth() end)
 hs.hotkey.bind(hyper, "K", function() hs.window.filter.focusNorth() end)
 hs.hotkey.bind(hyper, "L", function() hs.window.filter.focusEast() end)
-hs.hotkey.bind(hyper, "1", function() hs.application.launchOrFocus(terminal) end)
-hs.hotkey.bind(hyper, "2", function() hs.application.launchOrFocus(browser) end)
-hs.hotkey.bind(hyper, "3", function() hs.application.launchOrFocus(git) end)
-hs.hotkey.bind(hyper, "4", function() hs.application.launchOrFocus(notes) end)
-hs.hotkey.bind(hyper, "5", function() hs.application.launchOrFocus(empty) end)
-hs.hotkey.bind(hyper, "6", function() hs.application.launchOrFocus(empty) end)
-hs.hotkey.bind(hyper, "7", function() hs.application.launchOrFocus(empty) end)
-hs.hotkey.bind(hyper, "8", function() hs.application.launchOrFocus(empty) end)
-hs.hotkey.bind(hyper, "9", function() hs.application.launchOrFocus(chat) end)
-hs.hotkey.bind(hyper, "0", function() hs.application.launchOrFocus(music) end)
 
--- Terminal
-hs.application.enableSpotlightForNameSearches(true)
-terminalHidden = false
-hs.hotkey.bind({"cmd"}, "Escape", function()
-  local app = hs.application.find(terminal)
-  local hasFocus = app and app:isFrontmost()
-  hs.application.launchOrFocus(terminal)
-  if hasFocus and not TerminalHidden then
-    terminalHidden = true
-    app:hide()
-  else
-    terminalHidden = false
-    app:unhide()
+-- Toggle applications
+function toggleApp(appName)
+  appHidden = false
+  return function()
+    local app = hs.application.find(appName)
+    local hasFocus = app and app:isFrontmost()
+    hs.application.launchOrFocus(appName)
+    if hasFocus and not appHidden then
+      appHidden = true
+      app:hide()
+    else
+      appHidden = false
+      app:unhide()
+    end
   end
-end)
+end
+
+hs.hotkey.bind({"cmd"}, "Escape", toggleApp(terminal))
+hs.hotkey.bind(hyper, "1", toggleApp(terminal))
+hs.hotkey.bind(hyper, "2", toggleApp(browser))
+hs.hotkey.bind(hyper, "3", toggleApp(git))
+hs.hotkey.bind(hyper, "4", toggleApp(notes))
+hs.hotkey.bind(hyper, "5", toggleApp(empty))
+hs.hotkey.bind(hyper, "6", toggleApp(empty))
+hs.hotkey.bind(hyper, "7", toggleApp(empty))
+hs.hotkey.bind(hyper, "8", toggleApp(empty))
+hs.hotkey.bind(hyper, "9", toggleApp(chat))
+hs.hotkey.bind(hyper, "0", toggleApp(music))
 
 -- Experimentation
 hs.hotkey.bind(hyper, "R", function()
