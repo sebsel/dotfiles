cd270c350c676ac67363691ac2b6cb73ab4fe4bf
Sebastiaan Andeweg <seb@seblog.nl>
2022-09-09 22:35:51 +0200

    a more terminal based git workflow
    As I use Vim as my editor, I work mainly from the terminal. For some
    reason however, I never really got comfortable with Git on the terminal.
    I still use a GUI for it: first Tower and now Fork. This all happened
    because I did not know how to stage separate lines of a file, which I
    could do in the GUI but not on the command line (there is a solution
    though: `git add -p`, among others).
    
    I stil dread merging and rebasing and merge conflicts in the terminal,
    but with Fugitive and my knowledge of Git internals I should be fine
    nowadays.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,25 +1,20 @@
 # === Git ===
 
-# note to self: there are also aliases from the oh-my-zsh plugin
-# github.com/robbyrussell/oh-my-zsh/blob/master/plugins/git/git.plugin.zsh
+function g {
+  # With arguments: acts like `git`
+  if [[ $# > 0 ]]; then
+    git $@
+  else
+    git status
+  fi
+}
+
+# Complete g like git
+compdef g=git
 
 alias gs="git status -bs"
-alias gl="git log"
-alias gcom="git checkout main"
-alias gcod="git checkout develop"
-alias gcor="git checkout release"
-alias gcof="git checkout \$(git branch --format \"%(refname:short)\" | fzf)"
-alias gaa="git add ."
-alias gc="EDITOR=vim git commit -v"
+alias gc="git commit -v"
 alias gp="git push"
 alias gpl="git pull"
-alias gd="git diff"
-alias gdc="git diff --cached"
 alias nah="git reset --hard && git clean -df"
 alias wip="git add . && git commit -m \"wip\""
-alias git-hash="git rev-parse --short HEAD"
-alias git-init="git init -b main && gaa && git commit -q"
-
-#=== Git Tower ===
-alias tower="gittower"
-
