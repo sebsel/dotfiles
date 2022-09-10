4adce585e6fa33b518f4f023cc2d2f132b61baa1
Sebastiaan Andeweg <seb@seblog.nl>
2022-09-10 10:26:51 +0200

    add new Tmux switcher
    This one just lists all folders in ~/code/ and looks for a file named
    .tmux-session-name in the root. The contents of this file are used to
    name the session.
    
    If the session does not exist, it is created with that filename, with
    the folder opened. If it does exist we just switch to it.
    
    Added the .tmux-session-name to the .gitignore_global, so it will never
    be added to these git repos.
    
    Also adds a special case for the dotfiles, as I don't store them in my
    ~/code/ folder but directly as ~/dotfiles.

--- tmux/aliases.zsh
+++ tmux/aliases.zsh
@@ -20,3 +20,36 @@ function ftwindow() {
     tmux switch-client -t "$target_session"
   fi
 }
+
+# Project Switcher, based on the above, but looking at folders
+function ftproject() {
+  local projects session session_name session_folder
+
+  projects=$(
+    ls -l ~/code/ | awk '$1 ~ /^d/ {print $9}' |
+    while read project; do
+      if [[ -f "$HOME/code/$project/.tmux-session-name" ]]; then
+        echo "$(cat $HOME/code/$project/.tmux-session-name),$project"
+      else
+        echo "$project,$project"
+      fi
+    done; echo "dotfiles,../dotfiles"
+  )
+
+  session=$(echo "$projects" | xsv table | fzf --select-1 --exit-0 --prompt="tmux > ")
+  session_name=$(echo $session | awk '{ print $1 }')
+  session_folder=$(echo $session | awk "{ print \"$HOME/code/\" \$2 }")
+
+  tmux has-session -t $session_name 2>/dev/null
+
+  if [ $? != 0 ]; then
+    tmux new-session -d -s $session_name -c "$session_folder"
+  fi
+
+  # Attach to created session
+  if [ -z "$TMUX" ]; then
+    tmux attach-session -t "$session_name"
+  else
+    tmux switch-client -t "$session_name"
+  fi
+}
