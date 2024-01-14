e7ab4e8cd7f9c0667e10679f559985934d00211f
Sebastiaan Andeweg <seb@seblog.nl>
2024-01-14 15:03:06 +0100

    awesome layout tweaks

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -188,7 +188,17 @@ awful.screen.connect_for_each_screen(function(s)
     s.mytaglist = awful.widget.taglist {
         screen  = s,
         filter  = awful.widget.taglist.filter.all,
-        buttons = taglist_buttons
+        buttons = taglist_buttons,
+        widget_template = {
+          {
+            { id = 'text_role', widget = wibox.widget.textbox },
+            left = 7,
+            right = 7,
+            widget = wibox.container.margin
+          },
+          id = 'background_role',
+          widget = wibox.container.background,
+        },
     }
 
     -- Create a tasklist widget
@@ -217,7 +227,7 @@ awful.screen.connect_for_each_screen(function(s)
             id = 'background_role',
             widget = wibox.container.background,
           },
-          margins = 2,
+          margins = 3,
           widget = wibox.container.margin,
         },
     }
